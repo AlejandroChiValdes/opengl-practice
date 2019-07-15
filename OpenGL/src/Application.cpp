69101cff3afd3940672f4eeb3bd564dbb47839f3
unknown <alejandrochivaldes1@gmail.com>
2019-07-14 22:21:42 -0700

    Added basic imgui functionality

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -14,7 +14,21 @@
 #include "Texture.h"
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
 
+constexpr auto screenHeight = 960;
+constexpr auto screenWidth = 540;;
+
+void shiftX(float newPositionOffset, float* positions, float* raw_positions)
+{
+	int offset = 4;
+	for (int i = 0; i < 4; ++i)
+	{
+		positions[i * offset] = raw_positions[i * offset] + screenWidth * newPositionOffset;
+	}
+}
 
 int main(void)
 {
@@ -25,7 +39,8 @@ int main(void)
 		return -1;
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+
+	window = glfwCreateWindow(screenHeight, screenWidth, "Hello World", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
@@ -63,11 +78,19 @@ int main(void)
 	//	 -0.5f, 0.5f, 0.0f, 1.0f// 3
 	//};
 
-	float positions[] =
+	float raw_positions[] =
 	{
 		100.0f, 100.0f, 0.0f, 0.0f,
 		200.0f, 100.0f, 1.0f, 0.0f,
 		200.0f, 200.0f, 1.0f, 1.0f,
+		100.0f, 200.0f, 0.0f, 1.0f
+	};
+
+	float positions[] =
+	{
+		100.0f, 100.0f, 0.0f, 0.0f,
+		300.0f, 100.0f, 1.0f, 0.0f,
+		300.0f, 200.0f, 1.0f, 1.0f,
 		100.0f, 200.0f, 0.0f, 1.0f
 	};
 
@@ -94,9 +117,7 @@ int main(void)
 	/*glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);*/
 	glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
 	glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
-	glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
 
-	glm::mat4 mvp = proj * view * model;
 	// binds our shader
 	Shader shader("res/shaders/Basic.shader");
 	shader.Bind();
@@ -105,7 +126,7 @@ int main(void)
 	texture.Bind();
 	// Specify the slot of the texture that we want to use in our shader program
 	shader.SetUniform1i("u_Texture", 0);
-	shader.SetUniformMat4f("u_MVP", mvp);
+	
 	
 	// Uniforms are differentiated by how many items they contain and what type of items they contain.
 	// 4f: 4 floats
@@ -123,27 +144,83 @@ int main(void)
 
 	Renderer renderer;
 
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+
+	ImGui::StyleColorsDark();
+
+	// Setup Platform/Renderer bindings
+	const char* glsl_version = "#version 150";
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init(glsl_version);
+
+	bool show_demo_window = true;
+	bool show_another_window = false;
+	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+
+
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
 		renderer.Clear();
+
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+
+
 		shader.Bind();
 		shader.SetUniform4f("u_Color", red, green, blue, 1.0);
-		renderer.Draw(va, ib, shader);
 		
-		//inc = red <= 0.0f ? 0.5 : 
-		//	  red >= 1.0f ? -0.05f : 
-		//	  inc;
-		//red = red + inc;
-		// std::cout << "red: " << red << std::endl;
-		// blue = blue < 0.05f ? blue + inc : blue - inc;
-		//green = green > 0.95f ? green - inc : green + inc;
+
+
+		static float xDelta = 0.0f;
+		static float yDelta = 0.0f;
+
+		// 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+		{
+			static int counter = 0;
+			ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+			ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+			ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+			ImGui::Checkbox("Another Window", &show_another_window);
+
+			ImGui::SliderFloat("floatX", &xDelta, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+			ImGui::ColorEdit3("clear color", (float*)& clear_color); // Edit 3 floats representing a color
+			ImGui::SliderFloat("floatY", &yDelta, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+
+			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+			ImGui::End();
+		}
+
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		//shiftX(f, positions, raw_positions);
+		glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(xDelta * screenWidth, yDelta * screenHeight, 0));
+		glm::mat4 mvp = proj * view * model;
+		shader.SetUniformMat4f("u_MVP", mvp);
+		renderer.Draw(va, ib, shader);
+
+		for (int i = 0; i < 16; ++i)
+			std::cout << positions[i] << " ";
+		std::cout << std::endl;
+
+		
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
 
 		/* Poll for and process events */
 		glfwPollEvents();
 	}
+
+	// Cleanup
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
 
 	glfwTerminate();
 	return 0;
